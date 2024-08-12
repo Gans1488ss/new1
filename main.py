a456d44fe8c80609979b2e030eb36359ff1a45ef
Gans1488ss <dimasik1922ss@mail.ru>
2024-08-12 14:57:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+print(5*(9 ** 0.5))   #1th program
+print(9.99>9.98, 1000 != 1000.1)    #2th program
+print((2*2+2) == (2*(2+2)))    #3th program
+print(float('123.456')*10)  #4th program
+print(int(1234.56 % 10))
